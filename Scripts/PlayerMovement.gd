495ff89e4dc91c893a8e5fa709b54f9ba539b7f9
Cwdearth <77296394+Cwdearth@users.noreply.github.com>
2024-11-26 10:55:50 -0600

    Updated Player Controller
    Updated player controller behavior to be more appropriate for the game.

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -1,20 +1,21 @@
 extends CharacterBody3D
 
 const WALK_SPEED = 3.5
-const SENSITIVITY = 0.025
+const SENSITIVITY = 0.03
 
 @onready var head = $"Player Head"
 @onready var camera = $"Player Head/Camera3D"
 
 func _ready():
 	# Get rid of cursor
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-func _unhandled_input(event):
-	if event is InputEventMouseMotion:
-		head.rotate_y(-event.relative.x * SENSITIVITY)
-		camera.rotate_x(-event.relative.y * SENSITIVITY)
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(40))
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	return
+	
+func _process(delta):
+	if Input.is_action_pressed("left"):
+		head.rotate_y(SENSITIVITY)
+	elif Input.is_action_pressed("right"):
+		head.rotate_y(-SENSITIVITY)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -24,12 +25,15 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("up", "down", "right", "left")
-	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, 0)).normalized()
+
 	if direction:
 		velocity.x = direction.x * WALK_SPEED
 		velocity.z = direction.z * WALK_SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, WALK_SPEED)
 		velocity.z = move_toward(velocity.z, 0, WALK_SPEED)
+		
+	
 
 	move_and_slide()
